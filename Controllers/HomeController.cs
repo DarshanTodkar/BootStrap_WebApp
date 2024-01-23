69137e9020cc4af32c90cdc147cf5797b74bf177
admin <admin@DESKTOP-06R3QRR>
2024-01-23 18:39:47 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+using BootStrap_WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BootStrap_WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            List<Student> listStudents = new List<Student>()
+            {
+               new Student() { StudentId = 101, Name = "James", Branch = "CSE", Section = "A", Gender = "Male" },
+               new Student() { StudentId = 102, Name = "Smith", Branch = "ETC", Section = "B", Gender = "Male" },
+               new Student() { StudentId = 103, Name = "David", Branch = "CSE", Section = "A", Gender = "Male" },
+               new Student() { StudentId = 104, Name = "Sara", Branch = "CSE", Section = "A", Gender = "Female" },
+               new Student() { StudentId = 105, Name = "Pam", Branch = "ETC", Section = "B", Gender = "Female" }
+            };
+            return View(listStudents);
+        }
+
+        public ViewResult Details(int Id)
+        {
+            Student studentDetails = new Student() 
+            { 
+                StudentId = Id, Name = "James", Branch = "CSE", Section = "A", Gender = "Male" 
+            };
+            return View(studentDetails);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
